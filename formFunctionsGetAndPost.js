76b2d99b36195c1cfcee9135718263f3e93707e3
Elena <katsiashvili.elena@gmail.com>
2020-11-23 02:04:07 +0200

    Added Lesson_JS_09

---
+++ formFunctionsGetAndPost.js
@@ -0,0 +1,48 @@
+'use strict'
+
+// Get
+function sendDataByGetMethod() {
+	var userData = {
+			userName : document.getElementById('userName').value,
+			userSurname : document.getElementById('userSurname').value,
+			userAge : document.getElementById('userAge').value,
+			userAddress : document.getElementById('userAddress').value
+	};
+	
+	for (var value in userData)
+		userData[value] += '.ValidatedByGET';
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "/userGet?userName=" + userData.userName
+			+ "&userSurname=" + userData.userSurname + "&userAge="
+			+ userData.userAge + "&userAddress=" + userData.userAddress);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.send();	
+}
+
+
+// Post
+function sendDataByPostMethod() {
+	var userData = {
+			userName : document.getElementById('userName').value,
+			userSurname : document.getElementById('userSurname').value,
+			userAge : document.getElementById('userAge').value,
+			userAddress : document.getElementById('userAddress').value
+	};
+	
+	for (var value in userData)
+		userData[value] += '.ValidatedByPOST';
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "/userPost");
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.send(JSON.stringify(userData));	
+}
+
+// Validation
+userAge.onblur = function() {
+	if( this.value<1 || this.value>100 || isNaN(this.value)) {
+		this.value = "Error!!! Enter the age of 1 to 100";
+		userAge.focus();
+	}
+}
